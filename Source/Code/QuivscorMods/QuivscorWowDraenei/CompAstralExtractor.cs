a2a8553853855123767d25953dfe9a3eca6db1d3
Kacper Mierzejewski <quivscor@gmail.com>
2025-08-21 01:24:02 +0200

    Add initial release

---
+++ Source/Code/QuivscorMods/QuivscorWowDraenei/CompAstralExtractor.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RimWorld;
+using Verse;
+
+namespace QuivscorWowDraenei
+{
+    public class CompAstralExtractor : CompScanner
+    {
+        public new CompProperties_AstralExtractor Props
+        {
+            get
+            {
+                return this.props as CompProperties_AstralExtractor;
+            }
+        }
+
+        protected override void DoFind(Pawn worker)
+        {
+            IncidentParms parms = new IncidentParms();
+            parms.target = worker.Map;
+            parms.bypassStorytellerSettings = true;
+            parms.forced = true;
+            meteorites.def = Props.spawnedIncident;
+            meteorites.TryExecute(parms);
+        }
+
+        private IncidentWorker_MeteoriteImpact meteorites = new IncidentWorker_MeteoriteImpact();
+    }
+}
---
+++ Source/Code/QuivscorMods/QuivscorWowDraenei/CompProperties_AstralExtractor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RimWorld;
+using Verse;
+
+namespace QuivscorWowDraenei
+{
+    public class CompProperties_AstralExtractor : CompProperties_Scanner
+    {
+        public IncidentDef spawnedIncident;
+
+        public CompProperties_AstralExtractor()
+        {
+            this.compClass = typeof(CompAstralExtractor);
+        }
+    }
+}
